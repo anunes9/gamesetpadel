210e460ea8e0ec51fe128866d35e6a10d4d6a0fe
Andre Nunes <andrenunes.work@gmail.com>
2023-06-05 19:00:08 +0100

    🔍 Add PWA package

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.png'],
+      manifest: {
+        name: 'GameSetPadel',
+        short_name: 'MyApp',
+        description: 'Generate and manage your tournaments',
+        theme_color: '#53585f',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
